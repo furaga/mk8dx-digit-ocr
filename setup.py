d354d8f6fcb6f14068abb101b6dbbf0a955860bc
furaga <furaga.fukahori@gmail.com>
2023-05-04 00:52:22 +0900

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mk8dx_digit_ocr",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=[
         "opencv-python",
@@ -15,7 +15,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     author="furaga",
