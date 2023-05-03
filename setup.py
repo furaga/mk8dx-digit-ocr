748b5761a8482c329899cf3f7c4c915e561a764d
furaga <furaga.fukahori@gmail.com>
2023-05-04 01:28:16 +0900

    1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mk8dx_digit_ocr",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     install_requires=[
         "opencv-python",
