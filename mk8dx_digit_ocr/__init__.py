1c7ba87201dcda8e6e334fcb142df949bc1a339e
furaga <furaga.fukahori@gmail.com>
2023-05-03 19:37:24 +0900

    test code

--- mk8dx_digit_ocr/__init__.py
+++ mk8dx_digit_ocr/__init__.py
@@ -1,3 +1,4 @@
 from .digit_ocr import detect_digit
+
 __all__ = ["detect_digit"]
 VERSION = "1.0"
--- test/test_digit_ocr.py
+++ test/test_digit_ocr.py
@@ -1,13 +1,37 @@
-import pytest
+import sys
 import cv2
+
+sys.path.append(".")
 import mk8dx_digit_ocr
 
-def test_detect_digit():
-    img1 = cv2.imread("data/001.png", cv2.IMREAD_GRAYSCALE)
+
+def test_detect_digit_gray():
+    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_GRAYSCALE)
     ret, num = mk8dx_digit_ocr.detect_digit(img1)
-    assert ret and num, f"ret={ret}, num={num}"
+    assert ret and num == 20780, f"ret={ret}, num={num}"
 
-    img2 = cv2.imread("data/002.png", cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread("test/data/002.png", cv2.IMREAD_GRAYSCALE)
     ret, num = mk8dx_digit_ocr.detect_digit(img2)
-    assert ret and num, f"ret={ret}, num={num}"
+    assert ret and num == 9526, f"ret={ret}, num={num}"
 
+
+def test_detect_digit_bgr():
+    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_COLOR)
+    ret, num = mk8dx_digit_ocr.detect_digit(img1)
+    assert ret and num == 20780, f"ret={ret}, num={num}"
+
+    img2 = 255 - cv2.imread("test/data/002.png", cv2.IMREAD_COLOR)
+    ret, num = mk8dx_digit_ocr.detect_digit(img2)
+    assert ret and num == 9526, f"ret={ret}, num={num}"
+
+
+def test_detect_digit_bgra():
+    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_COLOR)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2BGRA)
+    ret, num = mk8dx_digit_ocr.detect_digit(img1)
+    assert ret and num == 20780, f"ret={ret}, num={num}"
+
+    img2 = 255 - cv2.imread("test/data/002.png", cv2.IMREAD_COLOR)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2BGRA)
+    ret, num = mk8dx_digit_ocr.detect_digit(img2)
+    assert ret and num == 9526, f"ret={ret}, num={num}"
