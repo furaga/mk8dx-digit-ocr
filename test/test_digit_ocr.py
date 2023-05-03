1ad05b2564f35b1b2ee123fd0afbbf5fb3ce0d24
furaga <furaga.fukahori@gmail.com>
2023-05-03 19:41:36 +0900

    refactor test code

--- test/test_digit_ocr.py
+++ test/test_digit_ocr.py
@@ -1,37 +1,31 @@
 import sys
 import cv2
+from pathlib import Path
 
 sys.path.append(".")
 import mk8dx_digit_ocr
 
 
 def test_detect_digit_gray():
-    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_GRAYSCALE)
-    ret, num = mk8dx_digit_ocr.detect_digit(img1)
-    assert ret and num == 20780, f"ret={ret}, num={num}"
-
-    img2 = cv2.imread("test/data/002.png", cv2.IMREAD_GRAYSCALE)
-    ret, num = mk8dx_digit_ocr.detect_digit(img2)
-    assert ret and num == 9526, f"ret={ret}, num={num}"
+    for img_path in Path("test/data").glob("*.png"):
+        img = cv2.imread(str(img_path), cv2.IMREAD_GRAYSCALE)
+        ret, num = mk8dx_digit_ocr.detect_digit(img)
+        gt = int(img_path.stem)
+        assert ret and num == gt, f"ret={ret}, num={num}, gt={gt}"
 
 
 def test_detect_digit_bgr():
-    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_COLOR)
-    ret, num = mk8dx_digit_ocr.detect_digit(img1)
-    assert ret and num == 20780, f"ret={ret}, num={num}"
-
-    img2 = 255 - cv2.imread("test/data/002.png", cv2.IMREAD_COLOR)
-    ret, num = mk8dx_digit_ocr.detect_digit(img2)
-    assert ret and num == 9526, f"ret={ret}, num={num}"
+    for img_path in Path("test/data").glob("*.png"):
+        img = cv2.imread(str(img_path), cv2.IMREAD_COLOR)
+        ret, num = mk8dx_digit_ocr.detect_digit(img)
+        gt = int(img_path.stem)
+        assert ret and num == gt, f"ret={ret}, num={num}, gt={gt}"
 
 
 def test_detect_digit_bgra():
-    img1 = 255 - cv2.imread("test/data/001.png", cv2.IMREAD_COLOR)
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2BGRA)
-    ret, num = mk8dx_digit_ocr.detect_digit(img1)
-    assert ret and num == 20780, f"ret={ret}, num={num}"
-
-    img2 = 255 - cv2.imread("test/data/002.png", cv2.IMREAD_COLOR)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2BGRA)
-    ret, num = mk8dx_digit_ocr.detect_digit(img2)
-    assert ret and num == 9526, f"ret={ret}, num={num}"
+    for img_path in Path("test/data").glob("*.png"):
+        img = cv2.imread(str(img_path), cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+        ret, num = mk8dx_digit_ocr.detect_digit(img)
+        gt = int(img_path.stem)
+        assert ret and num == gt, f"ret={ret}, num={num}, gt={gt}"
